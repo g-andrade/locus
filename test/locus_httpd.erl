13d8f095b9e98cb7096028d6c05c39c5c6e1e2f9
Guilherme Andrade <g@gandrade.net>
2018-02-11 19:17:43 +0000

    Remove trailing whitespace

--- test/locus_httpd.erl
+++ test/locus_httpd.erl
@@ -9,10 +9,10 @@ start() ->
                                     {port, 0},
                                     {server_root, "."},
                                     {document_root, "../../../../test/priv"},
-                                    {modules, 
+                                    {modules,
                                      [% https://github.com/simplegeo/erlang/blob/master/lib/inets/examples/server_root/conf/httpd.conf
                                       mod_alias, mod_auth, mod_esi, mod_actions, mod_cgi, mod_responsecontrol, mod_trace,
-                                      mod_range, mod_head, %mod_include, 
+                                      mod_range, mod_head, %mod_include,
                                       mod_dir, mod_get, mod_log, mod_disk_log
                                      ]}
                                    ]),
